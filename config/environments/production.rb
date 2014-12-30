5f7651d05c40ee0889943bc72d5d8ad8e1305935
Preetham Nadig <preethamnadig@yahoo.com>
2014-12-30 15:01:12 +0000

    changed region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
     :bucket => ENV['pindemo'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :region  => ENV['singapore']
     }
   }
 
