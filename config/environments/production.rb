5ccda51cf0be53aff39a1b3b1cb1c1f2a2eebd11
Preetham Nadig <preethamnadig@yahoo.com>
2014-12-30 14:35:22 +0000

    trying paperclip aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,13 @@ Rails.application.configure do
   
   
 
+  # config/initializers/paperclip.rb
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+
+  
+  
+
   
 end
